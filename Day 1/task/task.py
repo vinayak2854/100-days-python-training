128c1c812701d1b69886a9c94417ea606932d253
vinayak2854 <vinayak.vallakati@gmail.com>
2025-09-10 22:47:50 +0530

    project-1

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,8 @@
 # Write your code below this line 👇
+# print("Hello world!!")
+# print("Hello " + input("what is yoour name?"))
+print("welcome to the band opening")
+city = input("from which city from?\n")
+pet = input("what is your fav pet name?\n")
+
+print("your band could be " + city + " " + pet)
