395a60fe6b84ce47d04ae4236c0e35f8842ba7ce
vinayak2854 <vinayak.vallakati@gmail.com>
2025-09-14 16:18:10 +0530

    Add functionality to select a random friend and update state list with Mumbai and Dadar

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,12 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# option 1
+print(random.choice(friends))
+
+# option 2
+random_index = random.randint(0 , 4)
+
+print(friends[random_index])
+
---
+++ Day 4/Lists/task.py
@@ -0,0 +1,9 @@
+state_of_mh04 = ["bhiwandi", "thane","vinayak"]
+
+print(state_of_mh04[1]) 
+
+
+state_of_mh04[2] = "Mumbai"
+
+state_of_mh04.append("dadar")  
+print(state_of_mh04)
