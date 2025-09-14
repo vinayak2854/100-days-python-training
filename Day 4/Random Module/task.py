bbcd3a23c943c42d82649ec987da2cd42976eb5d
vinayak2854 <vinayak.vallakati@gmail.com>
2025-09-14 15:37:22 +0530

    Add my_module with favorite number and implement random heads or tails functionality

---
+++ Day 4/Random Module/task.py
@@ -0,0 +1,23 @@
+import random
+import my_module
+
+# random_integer = random.randint(1 , 100)
+# print(random_integer)
+
+# print(my_module.my_fav_num)
+
+# random_num_0_to_1 = random.random() * 5
+
+# print(random_num_0_to_1)
+
+
+# random_floating = random.uniform(1, 10)
+
+# print(random_floating)
+
+random_heads_or_tails = random.randint(0, 1)
+
+if random_heads_or_tails == 0:
+    print("heads")
+else:
+    print("tails")    
